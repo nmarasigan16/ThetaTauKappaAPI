3552bcb39391a801dd905bee5557b500b9ea424f
Nathan Marasigan <egarsenal16@gmail.com>
2016-05-17 22:32:01 -0500

    finished models

--- TTAPI/app/models.py
+++ TTAPI/app/models.py
@@ -5,7 +5,14 @@ from django.db import models
 #to extend the user class of rest_auth app
 from django.contrib.auth.models import User
 
-#chapter class
+"""
+Chapter class
+
+Has attributes:
+    -chapter_id
+    -chapter_name
+    -university
+"""
 class Chapter(models.Model):
     chapter_id = models.AutoField(primary_key=True)
     #TODO add more chapters if they want to use the API
@@ -15,8 +22,58 @@ class Chapter(models.Model):
     chapter_name = models.CharField(max_length=2, choices = CHAPTER_CHOICES, default='K')
     university = models.TextField()
 
+"""
+Event class
 
-#user class
+Has attributes:
+    -event_id (pk)
+    -date
+    -type (professional, philanthropy, brotherhood)
+    -location
+    -about
+"""
+class Event(models.Model):
+    event_id = models.AutoField(primary_key=True)
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.TextField()
+    about = models.TextField()
+
+
+"""
+Meeting class
+
+Has attributes:
+    -meeting_id
+    -mtype
+    -date
+"""
+class Meeting(models.Model):
+    meeting_id = models.AutoField(primary_key=True)
+    MEETING_TYPES = [
+            ('PM', 'Pledge Meeting'),
+            ('GM', 'General Meeting'),
+            ('VO', 'Voting'),
+            ('IN', 'Initiation'),
+    ]
+    mtype = models.CharField(max_length=2, choices=MEETING_TYPES, default='GM')
+    date = models.DateField()
+
+
+"""
+Class for a user
+
+Has attributes:
+    -user (to extend the rest_auth user)
+    -id (to identify user)
+    -name
+    -chapter_id (foreign key)
+    -year in school
+    -major
+    -current status
+    -whether or not they are an officer
+    -create_date (for analytics)
+"""
 class UserProfile(models.Model):
     #predefined user model
     user = models.OneToOneField(User)
@@ -62,15 +119,31 @@ class UserProfile(models.Model):
     ]
     status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='B')
 
+    events = models.ManyToManyField(Event)
+    meetings = models.ManyToManyField(Meeting)
+
     officer = models.BooleanField(default=False)
 
     create_date = models.DateField(auto_now_add=True)
 
     class Meta:
-        ordering = ['chapter', 'name']
+        ordering = ['chapter_id', 'name']
 
+"""
+Class for pledges
+Extends the UserProfile class
 
-#Pledge class
+Gives additional attributes:
+    -professional
+    -philanthropy
+    -social
+    -family
+    -brother
+    -pledge
+
+Each represents a number of signatures/events
+that a pledge needs to attend to become a brother
+"""
 class Pledge(models.Model):
     #extends user class
     user = models.OneToOneField(UserProfile, on_delete=models.CASCADE, primary_key=True)
@@ -83,15 +156,32 @@ class Pledge(models.Model):
     brother = models.PositiveIntegerField(default=0)
     pledge = models.PositiveIntegerField(default=0)
 
-#Brother class
+    #make it easy for historian to track all events
+
+"""
+Class for brothers
+Extends UserProfile class
+
+Gives additional attributes:
+    #used to let brothers track hours
+    -brotherhood
+    -philanthropy
+    -professional
+    -gms
+
+    #used for gm attendance, wiped out after scribe takes attendance
+    -attendance password
+    -excuse
+
+"""
 class Brother(models.Model):
     #extends user class
     user = models.OneToOneField(UserProfile, on_delete=models.CASCADE, primary_key=True)
 
     #event attendance
-    brotherhood = models.PositiveIntegerField(default=0)
-    philanthropy = models.PositiveIntegerField(default=0)
-    professional = models.PositiveIntegerField(default=0)
+    brotherhood = models.FloatField(default=0)
+    philanthropy = models.FloatField(default=0)
+    professional = models.FloatField(default=0)
 
     #gm stuff
     gms = models.PositiveIntegerField(default=0)
